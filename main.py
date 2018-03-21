ae8844bcac8dc55ed33bd0a75c625f5da238e5d3
ezra <linux.esdras@gmail.com>
2018-03-21 16:11:01 -0300

    Update main

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from block import Block
+from blockchain import Blockchain
 
 def main():
-    pass
+    bc = Blockchain()
 
+    bc.add_block('foo')
+    bc.add_block('bar')
+    bc.add_block('baz')
+    bc.add_block('qux')
+
+    for block in bc.chain:
+        print(block)
 
 if __name__ == '__main__':
     main()
